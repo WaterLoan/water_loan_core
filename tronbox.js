d415ab40da1f9a17149dff24b21e36dfe28e86b2
bibodeng <bibodeng@gmail.com>
2020-11-16 12:20:07 +0800

    add tronbox config

---
+++ tronbox.js
@@ -0,0 +1,50 @@
+const port = process.env.HOST_PORT || 9090
+
+module.exports = {
+  networks: {
+    mainnet: {
+      privateKey: process.env.PRIVATE_KEY_MAINNET,
+      /*
+Create a .env file (it must be gitignored) containing something like
+
+  export PRIVATE_KEY_MAINNET=4E7FECCB71207B867C495B51A9758B104B1D4422088A87F4978BE64636656243
+
+Then, run the migration with:
+
+  source .env && tronbox migrate --network mainnet
+
+*/
+      userFeePercentage: 100,
+      feeLimit: 1e9,
+      fullHost: 'https://api.trongrid.io',
+      network_id: '1'
+    },
+    shasta: {
+      privateKey: process.env.PRIVATE_KEY_SHASTA,
+      userFeePercentage: 50,
+      feeLimit: 1e9,
+      fullHost: 'https://api.shasta.trongrid.io',
+      network_id: '2'
+    },
+    nile: {
+      privateKey: process.env.PRIVATE_KEY_NILE,
+      userFeePercentage: 100,
+      feeLimit: 1e9,
+      fullHost: 'https://api.nileex.io',
+      network_id: '3'
+    },
+    development: {
+      // For trontools/quickstart docker image
+      privateKey: process.env.PRIVATE_KEY_DEVELOPMENT,
+      userFeePercentage: 0,
+      feeLimit: 1e9,
+      fullHost: 'http://127.0.0.1:' + port,
+      network_id: '9'
+    },
+    compilers: {
+      solc: {
+        version: '0.5.4'
+      }
+    }
+  }
+}
