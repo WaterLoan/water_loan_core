cd4662cc26433a92a8316c66c921110898ca335b
bibodeng <bibodeng@gmail.com>
2020-11-27 11:50:40 +0800

    add Liquidity Provider Reward

---
+++ contracts/tokenization/LpReward.sol
@@ -0,0 +1,192 @@
+import "../libraries/openzeppelin-solidity/contracts/math/Math.sol";
+import "../libraries/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "../libraries/openzeppelin-solidity/contracts/GSN/Context.sol";
+import "../libraries/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "../libraries/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "../libraries/openzeppelin-solidity/contracts/utils/Address.sol";
+
+
+contract IRewardDistributionRecipient is Ownable {
+    address rewardDistribution;
+
+    function notifyRewardAmount(uint256 reward) external;
+
+    modifier onlyRewardDistribution() {
+        require(_msgSender() == rewardDistribution, "Caller is not reward distribution");
+        _;
+    }
+
+    function setRewardDistribution(address _rewardDistribution)
+    external
+    onlyOwner
+    {
+        rewardDistribution = _rewardDistribution;
+    }
+}
+
+
+contract LPTokenWrapper {
+    using SafeMath for uint256;
+
+    IERC20 public lp;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function stake(uint256 amount) public {
+        lp.transferFrom(msg.sender, address(this), amount);
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+    }
+
+    function withdraw(uint256 amount) public {
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        lp.transfer(msg.sender, amount);
+    }
+}
+
+contract LpReward is LPTokenWrapper, IRewardDistributionRecipient {
+    IERC20 public save;
+    uint256 public DURATION = 100 days;
+
+    uint256 public initreward = 20800*1e6;
+    uint256 public starttime = 1602664326;
+    uint256 public periodFinish = 1612168325;
+    uint256 public rewardRate = 0;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+    mapping(address => uint256) public totalRewards;
+
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    function initContract(address _lockToken, address _mintToken, uint256 reward, uint _startTime, uint256 _duration) external onlyOwner {
+        initreward = reward;
+        starttime = _startTime;
+        DURATION = _duration;
+        periodFinish = starttime.add(DURATION);
+        save = IERC20(_mintToken);
+        lp = IERC20(_lockToken);
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return Math.min(block.timestamp, periodFinish);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (totalSupply() == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+        rewardPerTokenStored.add(
+            lastTimeRewardApplicable()
+            .sub(lastUpdateTime)
+            .mul(rewardRate)
+            .mul(1e18)
+            .div(totalSupply())
+        );
+    }
+
+    function earned(address account) public view returns (uint256) {
+        return
+        balanceOf(account)
+        .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+        .div(1e18)
+        .add(rewards[account]);
+    }
+
+    function stake(uint256 amount) public updateReward(msg.sender) checkhalve checkStart{
+        require(amount > 0, "Cannot stake 0");
+        super.stake(amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) public updateReward(msg.sender) checkhalve checkStart{
+        require(amount > 0, "Cannot withdraw 0");
+        super.withdraw(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function exit() external {
+        withdraw(balanceOf(msg.sender));
+        // getReward();
+    }
+
+    function getReward() public updateReward(msg.sender) checkhalve checkStart{
+        uint256 reward = earned(msg.sender);
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            save.transfer(msg.sender, reward);
+            totalRewards[msg.sender] = totalRewards[msg.sender].add(reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    modifier checkhalve(){
+        if (block.timestamp >= starttime) {
+            rewardRate = initreward.div(DURATION);
+            periodFinish = block.timestamp.add(DURATION);
+            emit RewardAdded(initreward);
+        }
+        _;
+    }
+    modifier checkStart(){
+        require(block.timestamp > starttime,"not start");
+        _;
+    }
+
+    function getRewardBalance() public view returns(uint){
+        return IERC20(save).balanceOf(address(this));
+    }
+
+    function notifyRewardAmount(uint256 reward)
+    external
+    onlyRewardDistribution
+    updateReward(address(0))
+    {
+        if (block.timestamp > starttime) {
+            if (block.timestamp >= periodFinish) {
+                rewardRate = reward.div(DURATION);
+            } else {
+                uint256 remaining = periodFinish.sub(block.timestamp);
+                uint256 leftover = remaining.mul(rewardRate);
+                rewardRate = reward.add(leftover).div(DURATION);
+            }
+            lastUpdateTime = block.timestamp;
+            periodFinish = block.timestamp.add(DURATION);
+            emit RewardAdded(reward);
+        } else {
+            rewardRate = reward.div(DURATION);
+            lastUpdateTime = starttime;
+            periodFinish = starttime.add(DURATION);
+            emit RewardAdded(reward);
+        }
+    }
+
+    function FetchWhenError() external onlyOwner {
+        save.transfer(owner(), getRewardBalance());
+    }
+}
